1d3eb7b28356358b87f02fbe08c81bc5acae5375
marajadesantelmo <50368116+marajadesantelmo@users.noreply.github.com>
2026-05-18 07:13:06 -0300

    clientes_nuevos_por_sucursal

--- clientes_nuevos.py
+++ clientes_nuevos.py
@@ -16,10 +16,16 @@ connection = mysql.connector.connect(
 cursor = connection.cursor()
 
 cursor.execute("""
-SELECT   e.Empresa, f.FechaEmision
+SELECT   e.Empresa, f.FechaEmision,
+    CASE 
+        WHEN t.NroSucursal IN (3, 8) THEN 'Bs.As.'
+        WHEN t.NroSucursal = 4 THEN 'Salta'
+        ELSE 'Otros'
+    END AS `Unidad de Negocios`
 FROM facturas f
 LEFT JOIN contactos c ON f.IDRef = c.IDContacto
-LEFT JOIN empresas e ON c.IDEmpresa = e.IDEmpresa     
+LEFT JOIN empresas e ON c.IDEmpresa = e.IDEmpresa
+LEFT JOIN talonarios t ON f.IDTalonario = t.RecID
 """)
 
 data = cursor.fetchall()
@@ -32,20 +38,21 @@ connection.close()
 
 facturas['FechaEmision'] = pd.to_datetime(facturas['FechaEmision'])
 facturas['AñoMes'] = facturas['FechaEmision'].dt.to_period('M')
-primeras_facturas = facturas.groupby('Empresa')['FechaEmision'].min().reset_index()
+idx = facturas.groupby('Empresa')['FechaEmision'].idxmin()
+primeras_facturas = facturas.loc[idx].reset_index(drop=True)
 primeras_facturas['AñoMes'] = primeras_facturas['FechaEmision'].dt.to_period('M')
-nuevos_clientes = primeras_facturas.groupby('AñoMes').size().reset_index(name='ClientesNuevos')
+nuevos_clientes = primeras_facturas.groupby(['AñoMes', 'Unidad de Negocios']).size().reset_index(name='ClientesNuevos')
 nuevos_clientes = nuevos_clientes.sort_values('AñoMes')
 nuevos_clientes['Mes'] = nuevos_clientes['AñoMes'].astype(str)
-nuevos_clientes= nuevos_clientes[['Mes', 'ClientesNuevos']]
+nuevos_clientes = nuevos_clientes[['Mes', 'Unidad de Negocios', 'ClientesNuevos']]
 #Guarda datos procedos para powerbi
 nuevos_clientes.to_excel('data/nuevos_clientes.xlsx')
 
 
 #Informe con nombre de cliente
-clientes_primer_factura = primeras_facturas[['Empresa', 'AñoMes']]
+clientes_primer_factura = primeras_facturas[['Empresa', 'AñoMes', 'Unidad de Negocios']]
 clientes_primer_factura['Mes'] = clientes_primer_factura['AñoMes'].astype(str)
-clientes_primer_factura = clientes_primer_factura[['Empresa', 'Mes']]
+clientes_primer_factura = clientes_primer_factura[['Empresa', 'Mes', 'Unidad de Negocios']]
 clientes_primer_factura = clientes_primer_factura.sort_values('Mes')
 
 # Save to Excel
